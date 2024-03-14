8fe65a81202b4ad27d2be76b15c21351f134fdbc
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-03-14 12:26:36 -0700

    Add sample test from https://github.com/jordimontana82/fake-xrm-easy-js-samples
    Adding a simple test to validate that basic tests work and there isn't some other issue.

---
+++ MarkMpn.FetchXmlToWebAPI.Tests/CreateContactTests.cs
@@ -0,0 +1,23 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MarkMpn.FetchXmlToWebAPI.Tests
+{
+    [TestClass]
+    public class CreateContactTests : FakeXrmEasyTestsBase
+    {
+        [TestMethod]
+        public async Task ShouldCreateContact()
+        {
+            var result = await CreateContactFn.CreateContact(Service, "Joe", "joe@satriani.com");
+            Assert.IsTrue(result.Succeeded);
+            
+            var contacts = Context.CreateQuery("contact").ToList();
+            Assert.AreEqual(1, contacts.Count);
+
+            Assert.AreEqual("Joe", contacts[0]["firstname"]);
+            Assert.AreEqual("joe@satriani.com", contacts[0]["emailaddress1"]);
+        }
+    }
+}
---
+++ MarkMpn.FetchXmlToWebAPI.Tests/FakeXrmEasyTestsBase.cs
@@ -0,0 +1,36 @@
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using FakeXrmEasy.Abstractions;
+using FakeXrmEasy.Abstractions.Enums;
+using FakeXrmEasy.FakeMessageExecutors;
+using FakeXrmEasy.Middleware;
+using FakeXrmEasy.Middleware.Crud;
+using FakeXrmEasy.Middleware.Messages;
+using Microsoft.PowerPlatform.Dataverse.Client;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MarkMpn.FetchXmlToWebAPI.Tests
+{
+    [TestClass]
+    [SuppressMessage("Design", "CA1051:Do not declare visible instance fields")]
+    public class FakeXrmEasyTestsBase
+    {
+        protected readonly IXrmFakedContext Context;
+        protected readonly IOrganizationServiceAsync2 Service;
+
+        public FakeXrmEasyTestsBase() 
+        {
+            Context = MiddlewareBuilder
+                            .New()
+                            .AddCrud()
+                            .AddFakeMessageExecutors(Assembly.GetAssembly(typeof(AddListMembersListRequestExecutor)))
+
+                            .UseCrud()
+                            .UseMessages()
+                            .SetLicense(FakeXrmEasyLicense.RPL_1_5)
+                            .Build();
+
+            Service = Context.GetAsyncOrganizationService2();
+        }
+    }
+}
