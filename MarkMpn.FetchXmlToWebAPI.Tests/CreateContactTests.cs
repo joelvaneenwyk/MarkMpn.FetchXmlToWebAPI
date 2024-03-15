50ae1539d98ad6da2d67344fd2d759e0e4e03b2b
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-03-15 08:48:02 -0700

    Simplify contact test and remove 'Microsoft.Azure.WebJobs*' package references

--- MarkMpn.FetchXmlToWebAPI.Tests/CreateContactTests.cs
+++ MarkMpn.FetchXmlToWebAPI.Tests/CreateContactTests.cs
@@ -1,12 +1,65 @@
 using System.Linq;
 using System.Threading.Tasks;
+using DataverseEntities;
+using JetBrains.Annotations;
+using Microsoft.PowerPlatform.Dataverse.Client;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Messages;
+using Task = System.Threading.Tasks.Task;
 
 namespace MarkMpn.FetchXmlToWebAPI.Tests;
 
 [TestClass]
 public class CreateContactTests : FakeXrmEasyTestsBase
 {
+    public class GenericResult
+    {
+        public bool Succeeded { get; private init; }
+        public string? ErrorMessage { get; set; }
+
+        public static GenericResult Succeed()
+        {
+            return new GenericResult
+            {
+                Succeeded = true,
+                ErrorMessage = ""
+            };
+        }
+    }
+
+    [PublicAPI]
+    public static class CreateContactFn
+    {
+        [PublicAPI]
+        public static async Task<GenericResult> CreateContact(IOrganizationServiceAsync2 service, string firstName,
+            string email)
+        {
+            await service.CreateAsync(new Contact
+            {
+                ["firstname"] = firstName,
+                ["emailaddress1"] = email
+            });
+
+            return GenericResult.Succeed();
+        }
+
+        [PublicAPI]
+        public static GenericResult CreateContactSync(IOrganizationService service, string firstName, string email)
+        {
+            service.Execute(new CreateRequest
+            {
+                Target =
+                    new Entity("contact")
+                    {
+                        ["firstname"] = firstName,
+                        ["emailaddress1"] = email
+                    }
+            });
+            return GenericResult.Succeed();
+        }
+    }
+
     [TestMethod]
     public async Task ShouldCreateContact()
     {
